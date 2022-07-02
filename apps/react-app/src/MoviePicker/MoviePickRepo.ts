2ce6eb67bea7db33d3ef6abbf00f12315f9f8f61
svadhis <imeian@gmail.com>
2022-07-03 01:25:42 +0200

    Updates

--- apps/react-app/src/MoviePicker/MoviePickRepo.ts
+++
@@ -1,8 +0,0 @@
-/**
- *
- */
-export interface MoviePickRepo {
-  getByFirstLetter: (firstLetter: string) => Promise<string | null>;
-  getAll: () => Promise<string[]>;
-  put: (title: string) => Promise<void>;
-}
